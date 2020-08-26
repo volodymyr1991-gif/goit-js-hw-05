cadc39836a717ab1f4521137a66dbbd7591ddec9
Volodymyr Molodynia <molodunja91@gmail.com>
2020-08-26 20:34:49 +0300

    first commit

--- task-3.js
+++ task-3.js
@@ -1,27 +1,73 @@
-const Storage=function(item){
-this.getItems=function(){
-    const total=item
-    return total
+class Storage {
+  constructor([...items]) {
+    this.items = items;
+
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(ter) {
+   this.items.push(ter)
+  }
+  removeItem(del){
+    for(let i=0; i<this.items.length; i+=1){
+      if(del===this.items[i]){
+        this.items.splice(i,1);
+      }
+    }
+  }
 }
 
- this.addItem=function(ttt){
-  total.push(ttt)
-}
-
-
-}
 const storage = new Storage([
-    'Нанітоіди',
-    'Пролонгер',
-    'Залізні жупи',
-    'Антигравітатор',
-  ]);
-  
-  const items = storage.getItems();
-  console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
-  
-  storage.addItem('Дроїд');
-  console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
-  
-//   storage.removeItem('Пролонгер');
-//   console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+  "Нанітоіди",
+  "Пролонгер",
+  "Залізні жупи",
+  "Антигравітатор",
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи","Антигравітатор" ]
+
+storage.addItem("Дроїд");
+console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+// number 5
+// class StringBuilder{
+//   constructor(value){
+// this._value=value
+//   }
+//   get value(){
+
+//     return this._value
+
+//   }
+
+//     append(str){
+//       this._value = `${this._value}${str}`
+//      }
+
+//    prepend(str){
+//     this._value=`${str}${this._value}`
+
+//    }
+
+//    pad(str){
+//     this._value=`${str}${this._value}${str}`
+//    }
+// }
+
+// const builder = new StringBuilder('.');
+
+// builder.append('^');
+// console.log(builder.value); // '.^'
+
+// builder.prepend('^');
+// console.log(builder.value); // '^.^'
+
+// builder.pad('=');
+// console.log(builder.value); // '=^.^='
---
+++ task-5.js
@@ -0,0 +1,112 @@
+class Car {
+
+    /*
+     * Додай статичний метод `getSpecs(car)`,
+     * який приймає об'єкт-машину як параметр і виводить
+     * в консоль значення властивостей maxSpeed, speed, isOn, distance и price.
+     */
+  
+    /*
+     * Конструктор отримує об'єкт налаштувань.
+     *
+     * Додай властивості майбутнього екземпляра класу:
+     *  speed - поточна швидкість, початкова 0
+     *  price - ціна автомобіля
+     *  maxSpeed - максимальна швидкість
+     *  isOn - заведений автомобіль, значення true або false. Спочатку false
+     *  distance - загальний кілометраж, спочатку 0
+     */
+    constructor({ speed = 0, price, maxSpeed, isOn = false, distance = 0 }) {
+        this.speed = speed;
+        this._price = price;
+        this.maxSpeed = maxSpeed;
+        this.isOn = isOn;
+        this.distance = distance;
+    }
+  get price(){
+      return this._price
+  }
+  set price(newPrice){
+     return this._price=newPrice
+  }
+    
+  
+    /*
+     * Додай код для того, щоб завести автомобіль
+     * Записує у властивість isOn значення true
+     */
+    turnOn() {
+         this.isOn=true;
+      
+    }
+  
+    /*
+     * Додай код для того, щоб заглушити автомобіль
+     * Записує у властивість isOn значення false,
+     * і скидає поточну швидкість в 0
+     */
+    turnOff() {
+       this.isOn=false;
+        this.speed=0;
+
+    }
+  
+    /*
+     * Додає до властивості speed отримане значення,
+     * за умови, що результуюча швидкість
+     * не більше, ніж значення властивості maxSpeed
+     */
+    accelerate(value) {
+        if(this.speed+value<=this.maxSpeed){
+            this.speed+=value
+        }
+
+    }
+  
+    /*
+     * Забирає від властивості speed отримане значення,
+     * за умови, що результуюча швидкість не менше нуля
+     */
+    decelerate(value) {
+        if(value<=this.speed)
+this.speed-=value
+    }
+  
+    /*
+     * Додає в поле distance кілометраж (hours * speed),
+     * але тільки в тому випадку, якщо машина заведена!
+     */
+    drive(hours) {
+        if(this.isOn){
+            this.distance=hours*this.speed+this.distance
+        }
+
+    }
+
+
+    static getSpecs(car){
+        console.log(car.maxSpeed,car.speed,car.isOn,car.distance, car.price)
+    }
+  }
+
+  const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+
+mustang.turnOn();
+
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
